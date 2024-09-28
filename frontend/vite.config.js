211c5a985bf409e40e9e17b4c2cad4c49888c3d6
sod3 <mudassir.muneer123@gmail.com>
2024-09-28 07:09:09 -0700

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-				changeOrigin: true,
-			},
-		},
-	},
-});
+  build: { manifest: true, outDir: './dist'},
+  base:  '/',
+  root: './src',
+  plugins: [react()],
+})
